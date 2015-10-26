fa037945ddf61133760bc0e754025e10551fd15c
Daniel Cruz <kohku76@gmail.com>
2015-10-26 15:59:25 -0600

    Troubleshooting deployment faiilure

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+ENV['CUSTOM_RUBY_VERSION'] || '2.1.7'
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,3 @@
-ruby ENV['CUSTOM_RUBY_VERSION'] || '2.1.7'
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
